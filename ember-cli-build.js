c18c062cbccaf16a3f47142abff6a14668d8b34b
Josh Justice <jjustice@bignerdranch.com>
2018-05-03 07:23:53 -0400

    Add ember-orbit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    orbit: {
+      packages: [
+        '@orbit/jsonapi',
+        '@orbit/indexeddb',
+        '@orbit/indexeddb-bucket',
+      ],
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
