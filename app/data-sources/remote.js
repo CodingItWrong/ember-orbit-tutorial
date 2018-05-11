280a08121ce265909e73de46bacd3a3764130511
Dan Gebhardt <dan@cerebris.com>
2018-05-11 12:13:49 -0400

    Bump orbit and ember-orbit dependencies
    Eliminates the need to ship ember-fetch and override `Orbit.fetch`.

--- app/data-sources/remote.js
+++ app/data-sources/remote.js
@@ -1,12 +1,8 @@
 import JSONAPISource from '@orbit/jsonapi';
 import Orbit from '@orbit/data';
-import fetch from 'fetch';
 
 export default {
   create(injections = {}) {
-    // Use `fetch` implementation from `ember-fetch`
-    Orbit.fetch = fetch;
-
     injections.name = 'remote';
     injections.host = 'http://localhost:3000';
 
